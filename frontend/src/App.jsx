07f92e013338b1d0114156730bca3948c959bfad
Krishna Sharma <krishnajan2004@gmail.com>
2025-04-02 02:34:35 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Notifications from "./pages/Notifications";
-import Settings from "./pages/Settings";
-import Search from "./pages/Search";
-import EditProfile from "./pages/EditProfile";
 import Login from "./pages/Login";
-import Profile from "./pages/Profile";
 import Navbar from "./components/Navbar";
 import Explore from "./pages/Explore";
 import Footer from "./components/Footer";
