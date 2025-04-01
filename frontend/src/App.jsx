c1b20d1d2a7b0f867b24c7d6933470a3b2698d1a
Krishna Sharma <krishnajan2004@gmail.com>
2025-04-02 02:38:15 +0530

    done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Login from "./pages/Login";
+import Profile from "./pages/Profile";
 import Navbar from "./components/Navbar";
 import Explore from "./pages/Explore";
 import Footer from "./components/Footer";
 import PostDetails from "./pages/PostDetails";
+import Login from "./pages/Login";
 
 const App = () => {
   return (
@@ -13,10 +14,6 @@ const App = () => {
     <Navbar />
     <Routes>  
       <Route path="/" element={<Home />} />
-      <Route path="/notifications" element={<Notifications />} />
-      <Route path="/settings" element={<Settings />} />
-      <Route path="/search" element={<Search />} />
-      <Route path="/edit-profile" element={<EditProfile />} />
       <Route path="/login" element={<Login />} />
       <Route path="/explore" element={<Explore />} />
       <Route path ="/home" element={<Home />} />
