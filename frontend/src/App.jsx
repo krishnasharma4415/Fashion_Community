2bc5005e442c5f303bfcb8e3bade88b10812bcda
Nafisa-0 <163779714+Nafisa-0@users.noreply.github.com>
2025-04-20 18:51:42 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,38 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Profile from "./pages/Profile";
-import Navbar from "./components/Navbar";
-import Explore from "./pages/Explore";
-import Footer from "./components/Footer";
-import PostDetails from "./pages/PostDetails";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
 
-const App = () => {
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from "./pages/Home.jsx";
+import Updates from "./pages/Updates.jsx"; 
+import Navbar from "./Components/Navbar.jsx";
+import Sidebar from "./Components/Sidebar.jsx";
+import PostCard from "./Components/PostCard.jsx";
+import Suggestions from "./Components/Suggestions.jsx";
+import Profile from "./pages/ProfilePage.jsx";
+import Explore from "./pages/Explore.jsx";
+import SearchAndFilters  from "./Components/SearchAndFilters.jsx";
+import UpdateCard from "./Components/UpdateCard.jsx"; 
+import PostCreation from "./pages/PostCreation.jsx"; 
+import './index.css';
+import './App.css'; // Import your CSS file here
+
+export default function App() {
   return (
-    <>
-    <Navbar />
-    <Routes>  
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/explore" element={<Explore />} />
-      <Route path ="/home" element={<Home />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/post/:id" element={<PostDetails />} />
-    </Routes>
-    <Footer />
-
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Navbar" element={<Navbar />} />
+        <Route path="/Sidebar" element={<Sidebar />} />
+        <Route path="/PostCard" element={<PostCard />} />
+        <Route path="/Suggestions" element={<Suggestions />} />
+        <Route path="/updates" element={<Updates />} />
+        <Route path="/Profile" element={<Profile />} />
+        <Route path="/Explore" element={<Explore />} />
+        <Route path="/SearchAndFilters" element={<SearchAndFilters />} />
+        <Route path="/UpdateCard" element={<UpdateCard />} />
+        <Route path="/PostCreation" element={<PostCreation />} />
+        {/* Add more routes as needed */}
+      </Routes>
+    </Router>
   );
-};
+} 
 
-export default App;
