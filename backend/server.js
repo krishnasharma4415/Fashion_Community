d0409bf849ab2c00547d4cd322ad6e897bfbe73c
Krishna Sharma <krishnajan2004@gmail.com>
2025-06-17 11:57:45 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -24,5 +24,10 @@ app.use("/api/likes", require("./routes/like"));
 app.use("/api/follows", require("./routes/follow"));
 app.use("/api/recommendations", require("./routes/recommendation"));
 
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
